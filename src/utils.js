5aa8b3992f0f71ce16e0024593a66c3d86b3d4fc
Leandro Ostera <leandro@ostera.io>
2016-10-15 18:51:39 +0200

    Adds high resolution timer to logs

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,6 @@
-const now = () => (new Date()).toTimeString().split(' ')[0]
+const _now_time = () => (new Date()).toTimeString().split(' ')[0]
+const tick = () => performance.now()|0
+const now = () => `${_now_time()}:${tick()}`
 
 const log = (...args: any[]): void => {
   (process.env.NODE_ENV !== "production")
