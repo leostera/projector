cb35c301012c5040fe5bb2fb337419910395034e
Leandro Ostera <leandro@ostera.io>
2016-10-23 13:11:30 +0200

    Use _meta.Env and annotate pluck

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,20 @@
+import _meta from 'projector/metadata'
+
 const _now_time = () => (new Date()).toTimeString().split(' ')[0]
 const tick = () => performance.now()|0
 const now = () => `${_now_time()}:${tick()}`
 
 const log = (...args: any[]): void => {
-  (process.env.NODE_ENV !== "production")
+  (_meta.Env !== "production")
     && console.log(now(), ...args)
 }
 
-log.ns = (namespace) => log.bind({}, namespace)
+log.ns = (namespace: string): void => log.bind({}, namespace)
 
 const error: Function = log.ns("ERROR:")
+const info:  Function = log.ns("INFO:")
 
-const pluck = (key) => {
+const pluck = (key: string): Function => {
   const keys = key.split(".")
   const traverse = (obj, keys) => {
     const k = keys.shift()
@@ -22,7 +25,6 @@ const pluck = (key) => {
     return traverse(obj, keys)
   }
 }
-
 
 export {
   log,
