36c10bb79f705dfe13fcccc1624a47917872baf9
Leandro Ostera <leandro@ostera.io>
2016-10-23 13:12:05 +0200

    Split up React components - import types

--- src/render.js
+++ src/render.js
@@ -1,51 +1,40 @@
 //@flow
 import { log, error } from 'projector/utils'
 
-import type { State } from 'projector/Types'
+import type t from 'projector/Types'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Repo = () => (
-  <section className="repository">
-    <h2> tldr.jsx </h2>
-    <section className="milestone">
-      <h3> v3 </h3>
-      <section className="issues">
-        <section className="issue">
-          <h4>Needless horizontal scroll on smalls screens</h4>
-        </section>
-        <section className="issue">
-          <h4>Unigrid</h4>
-        </section>
-        <section className="issue">
-          <h4>Add night-mode by applying a top-level class that redefines the color palette</h4>
-        </section>
-      </section>
-    </section>
-    <section className="milestone">
-      <h3> v4 </h3>
-      <section className="issues">
-        <section className="issue">
-          <h4>Move to tldr.sh</h4>
-        </section>
-        <section className="issue">
-          <h4>Platform Detection</h4>
-        </section>
-        <section className="issue">
-          <h4>Command Suggestion</h4>
-        </section>
-      </section>
+const Issue = (issue) => (
+  <section key={issue.key} id={issue.id} className="issue">
+    <h4>{issue.title}</h4>
+  </section>
+)
+
+const Milestone = (milestone: t.Milestone) => (
+  <section key={milestone.id} id={milestone.name} className="milestone">
+    <h3> {milestone.title} </h3>
+    <section className="issues">
+      { milestone.issues.map(Issue) }
     </section>
   </section>
 )
 
-export default (state: State): void => {
+const Repo = (repo: t.Repository) => (
+  <section key={repo.id} id={repo.name} className="repository">
+    <h2> {repo.name} </h2>
+    /*{ repo.milestones.map(Milestone) }*/
+  </section>
+)
+
+export default (state: t.State): void => {
   log("Renderer:", state)
+  const repos: [t.Repository] = state.get().map( Repo )
   try {
     ReactDOM.render((
       <section>
-        <Repo />
+         { repos }
       </section>
     ), document.getElementById('projector'))
   } catch (e) {
